136c10cefd7540c4e996efb2eb98c1ece386e80b
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2021-01-02 16:16:39 -0300

    +2 projects

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -4,3 +4,35 @@ const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+if(embaixo == 'impressora')
+{
+    pegarDe(baixo)
+    darPara(cima)
+}
+else if(embaixo == 'triturador')
+{
+    if(meuItem != 'vazio')
+    {
+        pegar(esquerda)
+        pegar(direita)
+        darPara(baixo)
+    }
+}
+else if(embaixo == 'buraco')
+{
+    if(itemEsquerda >= 50)
+    {
+        pegarDe(esquerda)
+    }
+    else
+    }
+    {
+        pegarDe(direita)
+    }
+}
+else
+{
+    pegarDe(baixo)
+}
+
